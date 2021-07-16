7461480e897b808d02b8a6de4ab2f31f5a04179e
Chanunchida <yok.8.12.2543@gmail.com>
2021-07-17 00:37:42 +0700

    function remove

--- index.js
+++ index.js
@@ -3,24 +3,47 @@ const inputButton = document.querySelector('.input-button')
 const content = document.querySelector('.content')
 
 
-function postcontent(){
-    Event.preventDefault();
+
+
+function postcontent(event){
+
+    event.preventDefault();
 
     const post = document.createElement('div');
     post.classList.add('post')
 
 
     const newPost = document.createElement('div');
-    newPost.innerHTML = input.nodeValue;
+    newPost.innerHTML = input.value;
     newPost.classList.add('new-post')
-    post.appendChild(newPost)
+    post.append(newPost)
 
 
     const likeButton = document.createElement('button')
     likeButton.type = 'button'
     likeButton.classList.add('like-button')
-    likeButton.innerHTML ='<i class="fas fa-heart-circle"></i>';
+    likeButton.innerHTML ='<i class="fas fa-heart"></i>';
+    // likeButton.addEventListener('click',like)
+    post.append(likeButton)
 
-    const removeButton = document.createElement('')
+    const removeButton = document.createElement('button')
+    removeButton.type = 'button'
+    removeButton.classList.add('remove-button')
+    removeButton.innerHTML = '<i class="fas fa-trash"></i>'
+    removeButton.addEventListener('click',removeTodo)
+    post.append(removeButton)
+
+
+    content.prepend(post)
+
+    input.value="";
 
 }
+
+function removeTodo(event) {
+    event.target.parentNode.remove();
+  
+  }
+  
+
+inputButton.addEventListener('click', postcontent)
