c47388d7c159e2aec3e2e7cb2072e0de6fbec067
Chanunchida <yok.8.12.2543@gmail.com>
2021-07-20 14:01:53 +0700

    done

--- index.js
+++ index.js
@@ -23,17 +23,19 @@ function postcontent(event) {
   const countlike = document.createElement('h6');
   countlike.classList.add('count-like');
   post.append(countlike)
-  let count=0;
+  
+
   const likeButton = document.createElement('button');
   likeButton.type = 'button';
   likeButton.classList.add('like-button');
   likeButton.innerHTML = '<i class="fas fa-heart"></i>';
 
+  let count=0;
   likeButton.addEventListener('click', () =>{
     count++;
     const iconlike = document.createElement('button');
     iconlike.type = 'button'
-    countlike.innerHTML = '<i class="fab fa-gratipay"></i>'+count;
+    countlike.innerHTML = '<i class="fab fa-gratipay"></i>'+ count;
   });
   post.append(likeButton)
 
@@ -46,8 +48,9 @@ function postcontent(event) {
   buttonremove.classList.add('remove-button');
   buttonremove.innerHTML = '<i class="fas fa-trash"></i>';
   buttonremove.addEventListener('click', (event)=>{
-    event.target.parentNode.remove();
     
+    event.target.parentNode.remove();
+
   });
   post.append(buttonremove)
 
